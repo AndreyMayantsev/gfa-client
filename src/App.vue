ff5cae6436824fa2947ae7a1723923464e088336
Andrey Mayantsev <dumspiro2019@gmail.com>
2023-02-24 23:54:44 +0300

    Added small and extra-small class for advise. Updated random image url generation.

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,15 @@
   <q-drawer class="bg-grey-8 text-white" v-model="leftDrawerOpen" side="left" overlay behavior="mobile" elevated>
       <!-- drawer content -->
       <q-toggle v-model="everyHour" label="Совет каждый час" />
+      <div v-for="tag in adviceTags" :key="tag.id">
+        #{{ tag.name }}
+      </div>
   </q-drawer>
 
   <q-page-container>
 
         <q-img class = "bkgimg" v-bind:src="adviceImage">
-          <div class="absolute-full text-subtitle2 flex flex-center advice-font" v-on:click="viewAdvice">
+          <div v-bind:class="adviceFont" v-on:click="viewAdvice">
             {{ currentAdvice }}
           </div>
         </q-img>
@@ -47,46 +50,101 @@ export default {
       leftDrawerOpen: false,
       everyHour: false,
       currentAdvice: "ПОЛУЧИ СВОЙ СОВЕТ",
-      adviceImage: "https://cdn.quasar.dev/img/parallax2.jpg"
+      adviceImage: "https://cdn.quasar.dev/img/parallax2.jpg",
+      adviceTags: [],
+      adviceFont: "absolute-full text-subtitle2 flex flex-center advice-font"
     }
   },
 
   methods: {
-    showMenu() {
+    async showMenu() {
       this.leftDrawerOpen = !this.leftDrawerOpen;
+      this.adviceTags = await this.getAdviceTags();
+      console.log("Categories: " + JSON.stringify(this.adviceTags));
     },
 
     async getAdvice(url) {
-      let advice  = ''
+      let advice  = '';
       let response = await fetch(url);
-      //document.body.style.backgroundImage = 'url(' + this.getRandomImage(); + ')';
     
       if (response.ok) { 
         advice = await response.json();
+
         this.adviceImage = this.getRandomImage();
       } else {
+        advice = "Включи блять интернет!"
         alert("Ошибка HTTP: " + response.status);
       }
       return advice;
     },
 
     getRandomImage() {
-      let ImageUrl = "https://fucking-great-advice.ru/data/frontpage/";
+      let imageUrl = "https://fucking-great-advice.ru/data/frontpage/";
       let maxNumber = 70;
+      let randomImage = imageUrl + Math.floor(Math.random() * maxNumber + 1) + ".jpg";
 
-      console.log("Image: " + ImageUrl + Math.floor(Math.random() * maxNumber + 10) + ".jpg");
-      return ImageUrl + Math.floor(Math.random() * maxNumber + 1) + ".jpg";
+      if (randomImage != this.adviceImage) {
+        return randomImage;
+      } else {
+        return imageUrl + Math.floor(Math.random() * maxNumber + 1) + ".jpg";
+      }        
     },
 
     async viewAdvice() {
       let _currentAdvice = await this.getAdvice('http://fucking-great-advice.ru/api/random');
-      this.currentAdvice = _currentAdvice['text']
+      this.setAdviceFont(_currentAdvice['text']);
+      this.currentAdvice = _currentAdvice['text'];
     },
 
     async getAdviceTags() {
-      // https://fucking-great-advice.ru/api/v2/tags
+      let url = "https://fucking-great-advice.ru/api/v2/tags";
+      let categories  = {};
+      let response = await fetch(url);
+      //document.body.style.backgroundImage = 'url(' + this.getRandomImage(); + ')';
+    
+      if (response.ok) { 
+        categories = await response.json();
+      } else {
+        alert("Ошибка HTTP: " + response.status);
+      }
+      return categories.data;
+    },
 
-    }
+    isLongWordsContained(advice) {
+        let extraMaxAbc = 10;
+        let maxAbc = 8;
+        let isLongWordsContainedAdvice = "standart";
+        let adviceMass = advice.split(' ');
+        
+        for(let adv in adviceMass) {
+          if (adviceMass[adv].length > maxAbc & adviceMass[adv].length < extraMaxAbc) {
+            isLongWordsContainedAdvice = "small";
+            console.log("В совете есть длиное слово!");
+            return isLongWordsContainedAdvice;
+          } else if (adviceMass[adv].length > extraMaxAbc) {
+            isLongWordsContainedAdvice = "extra-small";
+            console.log("В совете есть ОЧЕНЬ длиное слово!");
+            return isLongWordsContainedAdvice;
+          } else {
+            isLongWordsContainedAdvice = "standart";
+          }
+        }
+        return isLongWordsContainedAdvice;
+      },
+
+      setAdviceFont(advice) {
+        let extraSmall = "absolute-full text-subtitle2 flex flex-center advice-font-extra-small";
+        let small = "absolute-full text-subtitle2 flex flex-center advice-font-small";
+        let standart = "absolute-full text-subtitle2 flex flex-center advice-font";
+
+        if(this.isLongWordsContained(advice) == "small") {
+          this.adviceFont = small;
+        } else if (this.isLongWordsContained(advice) == "extra-small"){
+          this.adviceFont = extraSmall;         
+        } else {
+          this.adviceFont = standart;
+        }
+      }
 
   }
   
@@ -126,6 +184,22 @@ export default {
 .advice-font {
   font-family: "swiss";
   font-size: 5.0rem;
+  line-height: .9;
+  letter-spacing: 0;
+  text-decoration-style: solid;
+  cursor: pointer;
+}
+.advice-font-small {
+  font-family: "swiss";
+  font-size: 3.8rem;
+  line-height: .9;
+  letter-spacing: 0;
+  text-decoration-style: solid;
+  cursor: pointer;
+}
+.advice-font-extra-small {
+  font-family: "swiss";
+  font-size: 3.0rem;
   line-height: .9;
   letter-spacing: 0;
   text-decoration-style: solid;
